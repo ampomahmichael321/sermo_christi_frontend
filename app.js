97d7ac2058fa67dce0e007e6c0ed895a88b0ef06
Bishop <ampomahmichael321@gmail.com>
2026-06-14 11:57:29 +0000

    added functionality to the search bar

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let filtered = data;
 const scripturesContainer = document.getElementById("scriptures-container");
 const bookSelector = document.getElementById("book-select");
 const segmentLabels = document.querySelectorAll('input[name="scope-filter"]');
+const aboutSection = document.querySelector(".about");
 
 function renderData(scripturesJSON) {
   let scripturesHtml = "";
@@ -21,26 +22,40 @@ function renderData(scripturesJSON) {
     `;
     scripturesHtml += html;
   });
-  scripturesContainer.innerHTML = scripturesHtml;
+  aboutSection.innerHTML = "";
+  scripturesContainer.innerHTML = '<p class="loader">Loading....<p>';
+
+  setTimeout(() => {
+    scripturesContainer.innerHTML = scripturesHtml;
+  }, 1000);
 }
 
 function filterByBook(bookName) {
-  filtered = data.filter((entry) => entry.book === bookName);
-  scripturesContainer.innerHTML = "";
-  renderData(filtered);
+  if (scripturesContainer.innerHTML !== "") {
+    filtered = data.filter((entry) => entry.book === bookName);
+    scripturesContainer.innerHTML = "";
+    renderData(filtered);
+  }
 }
+
 function filterByWord(word) {
   filtered = data.filter((entry) =>
     entry.KJVText.toLowerCase().includes(word.toLowerCase()),
   );
   renderData(filtered);
 }
+
 function getParables() {
-  let filteredCopy = filtered.filter((entry) => entry.parable === true); //Make a copy of the filtered varible so that you can go back to the previous filtered
-  renderData(filteredCopy);
+  if (scripturesContainer.innerHTML !== "") {
+    let filteredCopy = filtered.filter((entry) => entry.parable === true); //Make a copy of the filtered varible so that you can go back to the previous filtered
+    renderData(filteredCopy);
+  }
 }
 
-renderData(data);
+const startButton = document.querySelector(".start-button");
+startButton.addEventListener("click", () => {
+  renderData(data);
+});
 
 bookSelector.addEventListener("change", (event) => {
   const selectedBook = event.target.value;
@@ -59,4 +74,9 @@ segmentLabels.forEach((radio) => {
       renderData(filtered);
     }
   });
+});
+
+const searchInput = document.querySelector("#keyword-search");
+searchInput.addEventListener("input", () => {
+  filterByWord(searchInput.value);
 });
